8e0ce903db2575aec9708796a3f80460adddca6f
Marleyhub <cardoso.gab@outlook.com>
2026-04-11 18:04:02 -0300

    docs: comentaries

--- src/editor/TextEng.java
+++ src/editor/TextEng.java
@@ -1,25 +1,26 @@
 package editor;
 
 public class TextEng {
+    // Text Engine:
+    // Responsable for wrinting into the Document object
 
     private Document document;
 
+    // Constructors
     public TextEng(){
         this.document = new Document();
     }
 
+    // Methods
     public void write(String text) {
         document.append(text);
     }
-
     public String getText() {
         return document.getText();
     }
-
     protected Document getDocument(){
         return this.document;
     }
-
     protected void setDocument(Document d){
         this.document = d;
     }
