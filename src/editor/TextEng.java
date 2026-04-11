0cc1a56acd797f179a9af7d70ca593c68f44da87
Marleyhub <cardoso.gab@outlook.com>
2026-04-11 19:07:34 -0300

    feature: adding new methods

--- src/editor/TextEng.java
+++ src/editor/TextEng.java
@@ -1,10 +1,19 @@
 package editor;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class TextEng {
-    // Text Engine:
-    // Responsable for wrinting into the Document object
+    /*
+    Text Engine:
+    Responsable for handling user actions into the Document object
+    (write, delete, get), and memento implementation (undo, redo)
+    */
 
     private Document document;
+
+    private Deque<Memento> undoStack = new ArrayDeque<>(); // stores what have been writen
+    private Deque<Memento> redoStack = new ArrayDeque<>(); // stores what what have been deleted
 
     // Constructors
     public TextEng(){
@@ -12,10 +21,29 @@ public class TextEng {
     }
 
     // Methods
-    public void write(String text) {
+    public void write(String text){
+        save();
         document.append(text);
+        redoStack.clear();
     }
-    public String getText() {
+    public void delete(int start, int end) {
+        save();
+        document.delete(start, end);
+        redoStack.clear();
+    }
+    public void undo(){
+        if (!undoStack.isEmpty()){
+            redoStack.push(saveToMemento());
+            Memento prev = undoStack.pop();
+            restoreFromMemento(prev);
+        }
+    }
+    public void redo(){
+        if(!redoStack.isEmpty()){
+
+        }
+    }
+    public String getText(){
         return document.getText();
     }
     protected Document getDocument(){
